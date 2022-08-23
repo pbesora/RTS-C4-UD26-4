a9384dbe28f1a6d51825323daefb2691273cddd0
pbesora <pau_besora@hotmail.com>
2022-08-23 22:11:03 +0200

    Project created

---
+++ src/main/java/com/example/demo/RtsC4Ud264Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtsC4Ud264Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtsC4Ud264Application.class, args);
+	}
+
+}
